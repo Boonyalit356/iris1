18a75960b53dfd0c7ed9f0737d42b9910198fa5f
Boonyalit356 <boonyalit.g@ku.th>
2025-07-29 15:55:52 +0700

    new initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Boonyalit356/iris1
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,11 @@
+package iris1
+
+import "fmt"
+
+func iris_say() {
+	fmt.Println("Iris say hello")
+}
+
+func iris_say2() {
+	fmt.Println("Iris say hello hello")
+}
